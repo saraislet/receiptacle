0695ff8a01670d9799e19a2ae971e1b10ce7cb5c
Sarai <anna.sarai.rosenberg@gmail.com>
2017-10-19 17:51:27 -0700

    prepare heroku test

---
+++ settings.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 18:17:58 2017
+
+@author: Sarai
+"""
+
+DOMAIN = {'people': {}}
--- sign_in.py
+++ sign_in.py
@@ -9,7 +9,9 @@ import os, re
 from flask import Flask, request, render_template, redirect, session
 import flask
 import tweepy
-import Sturmtest as st
+import pymysql.cursors
+#import Sturmtest as st
+import config_local as config
 
 app = Flask(__name__)
 
@@ -17,6 +19,18 @@ consumer_key = os.environ['consumer_key']
 consumer_secret = os.environ['consumer_secret']
 
 callback_url = 'https://murmuring-wildwood-21076.herokuapp.com/verify'
+
+
+def db_connect():
+    # Connect to the database
+    connection = pymysql.connect(host = os.environ['host'],
+                                 user = os.environ['user'],
+                                 password = os.environ['password'],
+                                 db = os.environ['db'],
+                                 charset = 'utf8mb4',
+                                 cursorclass = pymysql.cursors.DictCursor)
+    
+    return connection    
 
 
 def get_api():
@@ -30,7 +44,8 @@ def get_api():
         
         return tweepy.API(auth)
     except tweepy.TweepError:
-        return 'Error! Failed to build OAuthHandler!'
+        print('Error! Failed to build OAuthHandler!')
+        return
 
 
 @app.route('/')
@@ -52,8 +67,6 @@ def send_token():
         # TODO: Ideally, this would return an error message. Test this.
         return flask.render_template('error.html',
                                      error_msg = error_msg)
-
-
 
 
 @app.route("/verify")
@@ -96,7 +109,128 @@ def get_verification():
                                      error_msg = error_msg)
 
 
+@app.route("/receipts")
+def receipts():
     
+    api_user = get_api()
+    connection = db_connect()
+    
+    if api_user == None:
+        return flask.render_template('error.html')
+    
+    user_searched = request.form['screen_name']
+    results = []
+    
+    if user_searched != None and user_searched != "":
+        # Remove @ from username, if it exists.
+        # TODO: add parsing to identify user from twitter URLs (see NaziBlockBot)
+        # TODO: add error handling here if user enters bad URL
+        user_searched = re.sub(r"@","",user_searched)
+        
+        try:    
+            with connection.cursor() as cursor:
+                # Read a single record
+                sql = "SELECT * FROM `receipts` LIMIT 20"
+                cursor.execute(sql,)
+                results = cursor.fetchall()
+                
+                # If a matching record exists, return result, otherwise return message.
+                if results == []:
+                    print("Results array is empty. Something went wrong.")
+                else:
+                    print("User searched is in the database.")
+                    
+        except BaseException as e:
+            print("Error in add_account()", e)
+
+    else: 
+        # Get most recent 20 receipts from db.
+        results = []
+        try:
+            with connection.cursor() as cursor:
+                # Read 20 records
+                sql = "SELECT * FROM `receipts` LIMIT 20"
+                cursor.execute(sql,)
+                results = cursor.fetchall()
+                    
+        except BaseException as e:
+            print("Error in add_account()", e)
+    
+    
+    # Don't show list of results if there aren't any.
+    if results.len > 0:
+        show_results = True
+    else:
+        show_results = False
+    
+    return flask.render_template('results.html', 
+                             user_searched = user_searched,
+                             results = results.scores,
+                             num_results = results.len,
+                             show_results = show_results,
+                             logged_in = True)
+
+
+@app.route('/search', methods=['POST'])
+def search_user():
+    
+    api_user = get_api()
+    connection = db_connect()
+    
+    if api_user == None:
+        return flask.render_template('error.html')
+    
+    user_searched = request.form['screen_name']
+    results = []
+    
+    if user_searched != None and user_searched != "":
+        # Remove @ from username, if it exists.
+        # TODO: add parsing to identify user from twitter URLs (see NaziBlockBot)
+        # TODO: add error handling here if user enters bad URL
+        user_searched = re.sub(r"@","",user_searched)
+        
+        try:    
+            with connection.cursor() as cursor:
+                # Read a single record
+                sql = "SELECT * FROM `receipts` WHERE `screen_name`=%s LIMIT 20"
+                cursor.execute(sql, (user_searched,))
+                results = cursor.fetchall()
+                
+                # If a matching record exists, return result, otherwise return message.
+                if results == []:
+                    print("User searched is not in the database.")
+                else:
+                    print("User searched is in the database.")
+                    
+        except BaseException as e:
+            print("Error in add_account()", e)
+
+    else: 
+        # Get most recent 20 receipts from db.
+        results = []
+        try:
+            with connection.cursor() as cursor:
+                # Read 20 records
+                sql = "SELECT * FROM `receipts` LIMIT 20"
+                cursor.execute(sql,)
+                results = cursor.fetchall()
+                    
+        except BaseException as e:
+            print("Error in add_account()", e)
+    
+    
+    # Don't show list of results if there aren't any.
+    if results.len > 0:
+        show_results = True
+    else:
+        show_results = False
+    
+    return flask.render_template('results.html', 
+                             user_searched = user_searched,
+                             results = results.scores,
+                             num_results = results.len,
+                             show_results = show_results,
+                             logged_in = True)
 
     
 
@@ -113,51 +247,51 @@ def main():
                                  followers_count = userdata_json['followers_count'])
 
 
-@app.route('/sturm', methods=['POST'])
-def sturm():
-    
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(session['key'], session['secret'])
-    api_user = tweepy.API(auth)
-    
-#    api_user = session['api']
-    user = request.form['screen_name']
-    num_results = request.form['num_results']
-    
-    # Set num_results to default 30 if not a number.
-    # TODO: Validate form before submit?
-    if num_results.isdigit():
-        num_results = int(num_results)
-    else:
-        num_results = 30
-        
-    # Remove @ from username, if it exists.
-    # TODO: add parsing to identify user from twitter.com URLs?
-    # TODO: Check if all status/user URLs begin with twitter.com/$screen_name.
-    # TODO: add error handling here if user enters bad URL
-    user = re.sub(r"@","",user)
-    
-    re_patterns = st.init(st.words)
-    st.set_api(api_user)
-    results = st.test_followers(user, re_patterns, num_results)
-    st.print_results(results.scores)
-    
-    # Don't show list of baddies if there aren't any.
-    if results.num_baddies > 0:
-        show_baddies = True
-    else:
-        show_baddies = False
-    
-    return flask.render_template('results.html', 
-                             user = user,
-                             baddies_names = results.baddies_names,
-                             baddies = results.baddies,
-                             results = results.scores,
-                             num_baddies = results.num_baddies,
-                             num_results = results.num_results,
-                             ratio = results.ratio,
-                             show_baddies = show_baddies,
-                             logged_in = True)
+#@app.route('/sturm', methods=['POST'])
+#def sturm():
+#    
+#    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+#    auth.set_access_token(session['key'], session['secret'])
+#    api_user = tweepy.API(auth)
+#    
+##    api_user = session['api']
+#    user = request.form['screen_name']
+#    num_results = request.form['num_results']
+#    
+#    # Set num_results to default 30 if not a number.
+#    # TODO: Validate form before submit?
+#    if num_results.isdigit():
+#        num_results = int(num_results)
+#    else:
+#        num_results = 30
+#        
+#    # Remove @ from username, if it exists.
+#    # TODO: add parsing to identify user from twitter.com URLs?
+#    # TODO: Check if all status/user URLs begin with twitter.com/$screen_name.
+#    # TODO: add error handling here if user enters bad URL
+#    user = re.sub(r"@","",user)
+#    
+#    re_patterns = st.init(st.words)
+#    st.set_api(api_user)
+#    results = st.test_followers(user, re_patterns, num_results)
+#    st.print_results(results.scores)
+#    
+#    # Don't show list of baddies if there aren't any.
+#    if results.num_baddies > 0:
+#        show_baddies = True
+#    else:
+#        show_baddies = False
+#    
+#    return flask.render_template('results.html', 
+#                             user = user,
+#                             baddies_names = results.baddies_names,
+#                             baddies = results.baddies,
+#                             results = results.scores,
+#                             num_baddies = results.num_baddies,
+#                             num_results = results.num_results,
+#                             ratio = results.ratio,
+#                             show_baddies = show_baddies,
+#                             logged_in = True)
 
 @app.route('/logout')
 def logout():
@@ -165,8 +299,10 @@ def logout():
     del session['request_token']
     return redirect('../')
 
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
     
 app.secret_key = '\n\x8d-\xd1"\xfa;EG`\xc1?|\xd5*\xeaO\x91\x0c\x0c\x1as\x1e<'
+# TODO: put a new random string in .env and update heroku env
