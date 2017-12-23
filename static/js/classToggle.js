8cc36ed9b1d7ed2a7df4226150d8019fd8a1292c
Sarai <anna.sarai.rosenberg@gmail.com>
2017-12-22 16:40:03 -0800

    remove old code

--- static/js/classToggle.js
+++ static/js/classToggle.js
@@ -1,9 +1,5 @@
 function classToggle() {
   var contents = document.querySelectorAll('td.contents');
-  // contents[0].classList.toggle('censor');
-  // for (content in contents) {
-  //   content.classList.toggle('censor');
-  // }
 
   for (var i = contents.length - 1; i >= 0; i--) {
     contents[i].classList.toggle('censor');
